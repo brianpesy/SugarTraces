ef91ec60cbce1d9de2517ffceeb9471653fd05c2
Brian Sy <briansy@Brians-MacBook-Air.local>
2019-10-07 15:48:13 +0800

    Added achievement functionality (no animation and details yet)

--- SugarTraces/AchievementsViewController.swift
+++ SugarTraces/AchievementsViewController.swift
@@ -13,6 +13,18 @@ class AchievementsViewController: UIViewController {
     var loggedAchievements = [Bool](repeating: false, count: 11)
     var loggedAchDates = [String](repeating: "", count: 11)
     
+    @IBOutlet weak var ach0: UIImageView!
+    @IBOutlet weak var ach1: UIImageView!
+    @IBOutlet weak var ach2: UIImageView!
+    @IBOutlet weak var ach3: UIImageView!
+    @IBOutlet weak var ach4: UIImageView!
+    @IBOutlet weak var ach5: UIImageView!
+    @IBOutlet weak var ach6: UIImageView!
+    @IBOutlet weak var ach7: UIImageView!
+    @IBOutlet weak var ach8: UIImageView!
+    @IBOutlet weak var ach9: UIImageView!
+    @IBOutlet weak var ach10: UIImageView!
+    
     func loadAchievements(){
         var savedAchievements = defaults.array(forKey: Keys.savedAchievements) as? [Bool] ?? [Bool]()
         loggedAchievements = savedAchievements
@@ -42,7 +54,41 @@ class AchievementsViewController: UIViewController {
         print(loggedAchDates)
         
         //Change the icons of each achievement according to the loggedAchievements and loggedAchDates, both of which are of indices 0-10, corresponding to a specific achievement
+        if (loggedAchievements[0] == true){
+            ach0.image = UIImage(named: "imgAch0")
+        }
+        if (loggedAchievements[1] == true){
+            ach1.image = UIImage(named: "imgAch1")
+        }
+        if (loggedAchievements[2] == true){
+            ach2.image = UIImage(named: "imgAch2")
+        }
+        if (loggedAchievements[3] == true){
+            ach3.image = UIImage(named: "imgAch3")
+        }
+        if (loggedAchievements[4] == true){
+            ach4.image = UIImage(named: "imgAch4")
+        }
+        if (loggedAchievements[5] == true){
+            ach5.image = UIImage(named: "imgAch5")
+        }
+        if (loggedAchievements[6] == true){
+            ach6.image = UIImage(named: "imgAch6")
+        }
+        if (loggedAchievements[7] == true){
+            ach7.image = UIImage(named: "imgAch7")
+        }
+        if (loggedAchievements[8] == true){
+            ach8.image = UIImage(named: "imgAch8")
+        }
+        if (loggedAchievements[9] == true){
+            ach9.image = UIImage(named: "imgAch9")
+        }
+        if (loggedAchievements[10] == true){
+            ach10.image = UIImage(named: "imgAch10")
+        }
         
+
         
     }
     
